aa055aef6f084162a9d6596d3f4be3263a56bd83
ahahahahah <kozyrnovaleksandr@gmail.com>
2025-12-10 21:36:46 +0300

    working http with docker

--- server/src/main.c
+++ server/src/main.c
@@ -19,31 +19,22 @@ int main(void)
     FILE *file = fopen("log.log", "w");
     struct console_log_data console_log_data = { .out = file };
     struct prefork_epoll_data prefork_epoll_data = { .child_num = 4, .static_dir = "static" };
-
     ret = log_create(&console_log_ops, &console_log_data, &slog);
     if (ret)
     {
         perror("log_create");
         return -1;
     }
-
     logger_set(&slog);
-
-    ret = server_create("127.0.0.1", 12345, &prefork_epoll_ops, &prefork_epoll_data, &server);
+    ret = server_create("0.0.0.0", 80, &prefork_epoll_ops, &prefork_epoll_data, &server);
     if (ret)
     {
         perror("server_create");
         return -1;
     }
-
     /* signal kill, interrupt into sigint_handler */
     signal(SIGINT, sigint_handler);
-
     server.sops->start(&server);
-
-    // log_free(&slog);
-
     logger_unset();
-
     return 0;
 }
